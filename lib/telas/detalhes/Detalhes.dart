1e91baf5a219329e131f5e6e4dcdd488a4d68a42
Daniel Cavalcante <danielrcavalcante@gmail.com>
2019-12-27 01:09:52 -0300

    home detalhes e navegacao basica

---
+++ lib/telas/detalhes/Detalhes.dart
@@ -0,0 +1,83 @@
+import 'package:flutter/material.dart';
+import '../../modelos/receita.dart';
+
+class Detalhes extends StatelessWidget {
+
+  final Receita receita;
+
+
+  Detalhes({Key key, @required this.receita}): super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return _construirDetalhes();
+  }
+
+  Widget _construirDetalhes(){
+    return Scaffold(
+      body: ListView(
+        children: <Widget>[
+          _construirImagemDetalhes(receita.foto),
+          _construirTituloDetalhes(receita.titulo),
+          _construirLinhaIconesDetalhes('${receita.porcoes} porções',receita.tempoPreparo),
+          _construirSubtituloDetalhes('Ingredientes'),
+          _construirTextoDetalhes(receita.ingredientes),
+          _construirSubtituloDetalhes('Modo de preparo'),
+          _construirTextoDetalhes(receita.modoPreparo),
+        ],
+      ),
+      appBar: _construirAppBar(),
+    );
+  }
+
+  Widget _construirImagemDetalhes(diretorioImagem){
+    return Image.asset(diretorioImagem);
+  }
+
+  Widget _construirTituloDetalhes(titulo){
+    return Center(
+      child:Text(titulo,style: TextStyle(
+      color: Colors.deepOrange, fontSize: 30))
+    );
+  }
+
+  Widget _construirLinhaIconesDetalhes(rendimento,tempoPreparo){
+    return Row(
+      children: <Widget>[
+        _construirColunaIconeDetalhes(Icons.restaurant, rendimento),
+        _construirColunaIconeDetalhes(Icons.timer, tempoPreparo)
+      ],
+    );
+  }
+
+  Widget _construirColunaIconeDetalhes(icone, texto){
+    return
+      Expanded(
+        child: Column(
+          children: <Widget>[
+            Icon(icone, color: Colors.deepOrange),
+            Text(texto, style: TextStyle(
+                color: Colors.deepOrange, fontWeight: FontWeight.bold)),
+          ],
+        ),
+      );
+  }
+
+  Widget _construirSubtituloDetalhes(subtitulo){
+    return Center(
+      child: Text(subtitulo, style: TextStyle(fontSize: 20)),
+    );
+
+  }
+
+  Widget _construirTextoDetalhes(texto){
+    return Container(
+      padding: EdgeInsets.all(16),
+      child: Text(texto),
+    );
+  }
+
+  Widget _construirAppBar(){
+    return AppBar(title:Text('Cozinhando em Casa'));
+  }
+}
---
+++ lib/telas/home/Home.dart
@@ -0,0 +1,112 @@
+import 'package:cozinhando_casa/telas/detalhes/Detalhes.dart';
+import 'package:flutter/material.dart';
+import 'dart:convert';
+import '../../modelos/receita.dart';
+import '../detalhes/Detalhes.dart';
+
+class Home extends StatefulWidget{
+  @override
+  State<StatefulWidget> createState() {
+    return new HomeState();
+  }
+
+}
+
+class HomeState extends State<Home> {
+  @override
+
+  Widget build(BuildContext context) {
+    return _construirHome();
+  }
+
+  Widget _construirHome(){
+    return Scaffold(
+      body:_construirListaCard(),
+      appBar:_construirAppBar(),
+    );
+  }
+
+  Widget _construirListaCard(){
+    return FutureBuilder(
+      future: DefaultAssetBundle
+        .of(context)
+        .loadString('assets/receitas.json'),
+      builder: (context, snapshot){
+        List<dynamic> receitas = json.decode(snapshot.data.toString());
+
+        return ListView.builder(
+          itemBuilder: (BuildContext context, int index){
+            Receita receita = Receita.fromJson(receitas[index]);
+
+            return _construirCard(receita);
+          },
+          itemCount: receitas == null ? 0 : receitas.length,
+        );
+      }
+
+    );
+  }
+
+  Widget _construirCard(receita) {
+    return GestureDetector(
+      onTap: (){
+        Navigator.push(context, MaterialPageRoute(
+          builder: (context) => Detalhes(receita: receita)
+        ));
+      },
+      child: SizedBox(
+          height: 300,
+          child: Card(
+              margin: EdgeInsets.all(16),
+              child: Column(
+                children: <Widget>[
+                  Stack(
+                    children: <Widget>[
+                      _contruirImagemCard(receita.foto),
+                      _construirGradientCard(),
+                      _construirTextoCard(receita.titulo)
+                    ],
+                  )
+                ],
+              )
+          )
+      ),
+    );
+  }
+
+  Widget _construirGradientCard(){
+    return Container(
+      height: 268,
+      decoration: BoxDecoration(
+        gradient: LinearGradient(
+          begin: FractionalOffset.topCenter,
+          end: FractionalOffset.bottomCenter,
+          colors:[
+            Colors.transparent,
+            Colors.deepOrange.withOpacity(0.7)
+          ]
+        )
+      ),
+    );
+  }
+
+  Widget _construirTextoCard(titulo){
+    return Positioned(
+        bottom: 10,
+        left:   10,
+        child:Text(titulo,style: TextStyle(
+          fontSize:20,
+          color: Colors.white
+        ))
+    );
+  }
+
+  Widget _contruirImagemCard(foto){
+    return Image.asset(foto,fit: BoxFit.fill, height: 268);
+  }
+
+
+  Widget _construirAppBar(){
+    return AppBar(title:Text('Cozinhando em Casa'));
+  }
+}
